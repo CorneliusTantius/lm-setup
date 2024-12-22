e4a4b2334dfb414ffd875f74164b6dae739d77da
corneliustantius <corneliustantius.ct@gmailcom>
2024-12-22 00:31:41 -0500

    init

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+sudo apt purge libreoffice* -y
+sudo apt purge firefox -y
+sudo apt purge thunderbird -y
+sudo apt purge warpinator -y
+sudo apt purge onboard -y
+sudo apt purge simple-scan -y
+sudo apt purge pix -y
+sudo apt purge hypnotix -y
+sudo apt purge mintwelcome -y
+sudo apt purge mintchat -y
+
+sudo apt install git
+
+dir=/etc/apt/sources.list.d/
+filename=$dir/google-chrome.list
+if [ ! $dir ]
+then
+	mkdir $dir
+fi
+
+if [ ! -f $filename ]
+then
+	touch $filename
+	sudo echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > $filename
+fi
+
+sudo apt install google-chrome-stable
